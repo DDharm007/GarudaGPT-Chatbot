bf373c5610a1499622b6a30b8fb90756e9e564fe
Dharm Patel <168700406+DDharm007@users.noreply.github.com>
2025-09-29 13:37:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # config.py
-GEMINI_API_KEY = "AIzaSyCzpSDoqxFC0fCzv3rlcF__L7nR1epsy1Y"
+GEMINI_API_KEY = "YOUR_GEMINI_API_(OPTIONAL)"
+
 GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent" 
